238f6b31fbf9241e80c36d36675b27b50a7408e0
sergio <calbosergio734@gmail.com>
2022-12-02 11:18:27 -0300

    modificaciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,11 @@
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
-  });
-  // Cerrar el menú de hambuguesa cuando se hace click en un enlace  
+let btn = document.querySelector('#scroll-up');
 
-// Seleccionar enlaces de navegación
-const navLink = document.querySelectorAll(".nav-link");
+btn.addEventListener('click',() => {
+document.body.scrollTo({
+    top:0
+})
 
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
+})
+document.documentElement.scrollTo({
+    top: 0
+})
